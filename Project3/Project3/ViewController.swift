992cd96b97a0ba5a0b4ff7355c9c7588f79b04ed
Zach Frew <zachary.frew@ascension-external.org>
2020-03-21 13:37:01 -0500

    Project 3 setup

---
+++ Project3/Project3/ViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+    }
+}
