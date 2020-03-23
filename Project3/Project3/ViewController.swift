f4f207c1af57b3a6dcb80f7cb377ad42bc9c0583
Zach Frew <zachary.frew@ascension-external.org>
2020-03-23 13:01:48 -0500

    Add reticule and track it with eye & face movement

--- Project3/Project3/ViewController.swift
+++ Project3/Project3/ViewController.swift
@@ -4,10 +4,14 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var reticule: UIImageView!
     
     let face = SCNNode()
     let leftEye = Eye(color: .red)
     let rightEye = Eye(color: .blue)
+    let phone = SCNNode(geometry: SCNPlane(width: 1, height: 1))
+    let smoothingAmount = 20
+    var eyeLookHistory = ArraySlice<CGPoint>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +19,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.delegate = self
         
         sceneView.scene.rootNode.addChildNode(face)
+        sceneView.scene.rootNode.addChildNode(phone)
         face.addChildNode(leftEye)
         face.addChildNode(rightEye)
     }
@@ -30,6 +35,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func update(using anchor: ARFaceAnchor) {
         leftEye.simdTransform = anchor.leftEyeTransform
         rightEye.simdTransform = anchor.rightEyeTransform
+        
+        let points = [leftEye, rightEye].compactMap { eye -> CGPoint?  in
+            let hitTest = phone.hitTestWithSegment(from: eye.target.worldPosition, to: eye.worldPosition)
+            return hitTest.first?.screenPosition
+        }
+        
+        guard let leftPoint = points.first, let rightPoint = points.last else { return }
+        
+        let centerPoint = CGPoint(x: (leftPoint.x + rightPoint.x) / 2, y: -(leftPoint.y + rightPoint.y) / 2)
+        reticule.transform = eyeLookHistory.averageTransform
+        
+        eyeLookHistory.append(centerPoint)
+        eyeLookHistory = eyeLookHistory.suffix(smoothingAmount)
     }
 }
 
@@ -40,5 +58,36 @@ extension ViewController: ARSessionDelegate {
             self.face.simdTransform = node.simdTransform
             self.update(using: faceAnchor)
         }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let pov = sceneView.pointOfView?.simdTransform else { return }
+        phone.simdTransform = pov
+    }
+}
+
+extension SCNHitTestResult {
+    var screenPosition: CGPoint {
+        let physicalSize = CGSize(width: 0.062 / 2, height: 0.135 / 2)
+        let screenResolution = UIScreen.main.bounds.size
+        let screenX = CGFloat(localCoordinates.x) / physicalSize.width * screenResolution.width
+        let screenY = CGFloat(localCoordinates.y) / physicalSize.height * screenResolution.height
+        
+        return CGPoint(x: screenX, y: screenY)
+    }
+}
+
+extension Collection where Element == CGPoint {
+    var averageTransform: CGAffineTransform {
+        var x: CGFloat = 0
+        var y: CGFloat = 0
+        
+        for item in self {
+            x += item.x
+            y += item.y
+        }
+        
+        let floatCount = CGFloat(count)
+        return CGAffineTransform(translationX: x / floatCount, y: y / floatCount)
     }
 }
