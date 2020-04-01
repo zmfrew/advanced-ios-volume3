7e4643fa6df2f4813be1b777dc53e1de2f36b9ef
Zach Frew <zachary.frew@ascension-external.org>
2020-04-01 13:15:26 -0500

    Display associate wikipedia webpage based on image

--- Project4b/Project4b/ViewController.swift
+++ Project4b/Project4b/ViewController.swift
@@ -54,8 +54,31 @@ final class ViewController: UIViewController, ARSCNViewDelegate {
         titleNode.pivotOnTopLeft()
         titleNode.position.x += Float(plane.width / 2) + spacing
         titleNode.position.y += Float(plane.height / 2)
-        
         planeNode.addChildNode(titleNode)
+        
+        let painterNode = textNode(painting.artist, font: UIFont.systemFont(ofSize: 8))
+        painterNode.pivotOnTopCenter()
+        painterNode.position.y -= Float(plane.height / 2) + spacing
+        planeNode.addChildNode(painterNode)
+        
+        let yearNode = textNode(painting.year, font: UIFont.systemFont(ofSize: 6))
+        yearNode.pivotOnTopCenter()
+        yearNode.position.y = painterNode.position.y - spacing - painterNode.height
+        planeNode.addChildNode(yearNode)
+        
+        let detailsWidth = max(titleNode.width, 0.25)
+        let detailsHeight = (Float(plane.height) - titleNode.height) + painterNode.height + yearNode.height + (spacing * 2)
+        let detailsPlane = SCNPlane(width: CGFloat(detailsWidth), height: CGFloat(detailsHeight))
+        let detailsNode = SCNNode(geometry: detailsPlane)
+        detailsNode.pivotOnTopLeft()
+        detailsNode.position.x += Float(plane.width / 2) + spacing
+        detailsNode.position.y = titleNode.position.y - titleNode.height - spacing
+        planeNode.addChildNode(detailsNode)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            SCNTransaction.animationDuration = 1
+            node.opacity = 1
+        }
         
         return node
     }
